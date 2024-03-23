58b1cd0c185ef92cb0660a19870905082744019d
Raman Svezhantsau <raman.svezhantsau@maruthree.com>
2024-03-23 17:07:44 +0000

    remove project example files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,8 @@
 import React from "react";
-import { GetStaticProps } from "next";
 import Layout from "../components/Layout";
-import Post, { PostProps } from "../components/Post";
 import WhiteBoard from "../components/WhiteBoard";
-import prisma from "../lib/prisma";
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   const feed = await prisma.post.findMany({
-//     where: { published: true },
-//     include: {
-//       author: {
-//         select: {
-//           name: true,
-//         },
-//       },
-//     },
-//   });
-  
-//   return {
-//     props: { feed },
-//     revalidate: 10,
-//   };
-// };
 
 type Props = {
-  feed: PostProps[];
 };
 
 const Blog: React.FC<Props> = (props) => {
